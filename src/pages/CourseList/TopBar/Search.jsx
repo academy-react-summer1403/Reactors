406661ebb6a5a2ad1453d0caf6deef583ee9e6d4
Tina <tinamahmoudi201@gmail.com>
2024-10-01 22:39:29 -0800

    -

--- src/pages/CourseList/TopBar/Search.jsx
+++ src/pages/CourseList/TopBar/Search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 const Search = ()=>{
     return(
@@ -11,7 +12,9 @@ const Search = ()=>{
             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
-            <span class="sr-only">Search</span>
+            <span class="sr-only">Search
+            <CiSearch />
+            </span>
         </button>
     </div>
 </div>
