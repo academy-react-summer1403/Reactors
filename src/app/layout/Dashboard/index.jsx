0c6e40ff74dbf8c698c7ede9ff604e05dff3ecd2
Parham-Khoshnazar <parham.khoshnazar@gmail.com>
2024-10-12 22:05:53 +0330

    update: Update dashboard layout

--- src/app/layout/Dashboard/index.jsx
+++ src/app/layout/Dashboard/index.jsx
@@ -15,31 +15,28 @@ import { Counter } from '../../../pages/Dashboard/Counter'
 
 const DashboardLayout = () => {
     return (
-        <>
-            <div className="w-full h-full flex text-[#22445D] text-[18px]">
-                <div className="w-[400px] h-full flex flex-col text-nowrap">
-                    <UserInformation />
-                    <OptionsHolder />
-                </div>
-                <div className="w-full h-full flex flex-col">
-                    <HeaderBody className="bg-[#A4F6DE] shadow-[0px_10px_3px_0_#00000024]">
-                        <H1Academy />
-                        <HeaderOptionsHolder>
-                            <HeaderOptions icon={<HiOutlineEyeDropper />} />
-                            <HeaderOptions icon={<HiOutlineMoon />} />
-                            <HeaderOptions icon={<HiOutlineShoppingBag />} span={<ShopBasketSapn> 3 </ShopBasketSapn>} />
-                            <UserProfile />
-                        </HeaderOptionsHolder>
-                    </HeaderBody>
-                    <div className="h-full p-5">
-                        <DashboardPageHolder>
-                            <Counter />
-                        </DashboardPageHolder>
-                    </div>
+        <div className="w-full h-full flex text-[#22445D] text-[18px]">
+            <div className="w-[400px] h-full flex flex-col text-nowrap">
+                <UserInformation />
+                <OptionsHolder />
+            </div>
+            <div className="w-full h-full flex flex-col">
+                <HeaderBody className="bg-[#A4F6DE] shadow-[0px_10px_3px_0_#00000024]">
+                    <H1Academy />
+                    <HeaderOptionsHolder>
+                        <HeaderOptions icon={<HiOutlineEyeDropper />} />
+                        <HeaderOptions icon={<HiOutlineMoon />} />
+                        <HeaderOptions icon={<HiOutlineShoppingBag />} span={<ShopBasketSapn> 3 </ShopBasketSapn>} />
+                        <UserProfile />
+                    </HeaderOptionsHolder>
+                </HeaderBody>
+                <div className="h-full p-5 overflow-y-scroll">
+                    <DashboardPageHolder>
+                        <Counter />
+                    </DashboardPageHolder>
                 </div>
             </div>
-
-        </>
+        </div>
     )
 }
 
---
+++ src/components/Dashboard/Dashboard Pages/Styled Dashboard Pages/StyledDashboardPageHolder.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DashboardPageHolder = styled.div({
+    border: "1px solid #E8E8E8",
+    borderRadius: "15px",
+    padding: "25px",
+    boxShadow: "10px 10px 5px 0 #00000029",
+})
