ab3f5ff3fcd30b2ede59ba700dd14063691e5fc8
Tina <tinamahmoudi201@gmail.com>
2024-10-14 15:44:28 -0800

    -

--- src/pages/BlogList/newsList.jsx
+++ src/pages/BlogList/newsList.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Header } from '../../components/common/Hearder/Header'
+import { Footer } from '../../components/common/Footer/Footer'
 
 const NewsList = () => {
   return (
     <div>
-        
+        <Header/>
+        {/* <Footer/> */}
+
     </div>
   )
 }
