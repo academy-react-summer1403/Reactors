4175ca14075b8658732f7ac8bb94e414b9618367
Tina <tinamahmoudi201@gmail.com>
2024-10-14 14:45:53 -0800

    -

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CoursLists } from '../components/CourseList'
-import { Header } from '../components/Header/header'
-import { Footer } from '../components/Footer/footer'
-import { Bar } from '../components/TopBar/bar'
-import CoursesList from '../pages/CoursesList'
+import "./App.css";
+import CoursesList from "../pages/CoursesList";
 function App() {
-
   return (
     <>
-      <Header/>
-      <CoursesList/>
-      <Footer/>
-
+      <CoursesList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
