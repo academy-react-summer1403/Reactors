b41e84e0594ef8350e647474ca52932ea31adaf0
Tina <tinamahmoudi201@gmail.com>
2024-10-15 11:58:48 -0800

    -

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+import React, { useRef, useState,useEffect } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { getSameNews } from "../../core/services/api/getSameNews";
+// import { Card } from "../Card/Card";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "./styles.css";
+
+export default function Slider() {
+  // const [sameNews, setSameNews] = useState([]);
+
+  // const getNews = async () => {
+  //   const sameNews = await getSameNews();
+  //   setSameNews(sameNews);
+  //   console.log(sameNews, "courseTop");
+  // };
+
+  // useEffect(() => {
+  //   getNews();
+  // }, []);
+
+  return (
+    <>
+      <Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper">
+        {sameNews.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Card data={item} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
+}
--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Header } from '../../components/common/Header/Header'
+import { Footer } from '../../components/common/Footer/Footer'
+import Slider from '../../components/Slider/Slider'
 
 const CourseDetails = () => {
   return (
     <>
       <Header/>
+      {/* <Slider/> */}
+      {/* <Footer/> */}
     </>
   )
 }
