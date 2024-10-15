8f065af136ccfa3b68088b55aac90623c3986842
Tina <tinamahmoudi201@gmail.com>
2024-10-15 13:53:11 -0800

    left-details-design

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Header } from '../../components/common/Header/Header'
-import { Footer } from '../../components/common/Footer/Footer'
-import Slider from '../../components/Slider/Slider'
+import React from "react";
+import { Header } from "../../components/common/Header/Header";
+import { Footer } from "../../components/common/Footer/Footer";
+import Slider from "../../components/Slider/Slider";
+import LeftDetails from "../../components/Left-Side-details/LeftDetails";
 
 const CourseDetails = () => {
   return (
     <>
-      <Header/>
+      <Header />
+      <div className="flex flex-row-reverse px-16 py-[70px]">
+        <LeftDetails />
+      </div>
+
       {/* <Slider/> */}
       {/* <Footer/> */}
     </>
-  )
-}
+  );
+};
 
-export default CourseDetails
+export default CourseDetails;
