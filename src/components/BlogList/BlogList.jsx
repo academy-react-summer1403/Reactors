f4416b776c25f34467f4054b0c9a9ef2eeceaa0b
Tina <tinamahmoudi201@gmail.com>
2024-10-14 16:03:44 -0800

    -

---
+++ src/components/BlogList/BlogList.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const BlogList = () => {
+  const [blogList, setblogList] = useState([]);
+
+  return (
+    <div>
+      <div className="flex flex-col min-h-[400px] bg-[#FFFFFF] gap-3 p-4 rounded-[30px] shadow-2xl">
+
+      </div>
+    </div>
+  );
+};
+
+export default BlogList;
--- src/pages/BlogList/newsList.jsx
+++ src/pages/BlogList/newsList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Header } from '../../components/common/Hearder/Header'
 import { Footer } from '../../components/common/Footer/Footer'
+import BlogList from '../../components/BlogList/blogList'
 
 const NewsList = () => {
   return (
     <div>
         <Header/>
         {/* <Footer/> */}
-
+        <BlogList/>
     </div>
   )
 }
