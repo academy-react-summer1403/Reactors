0358a061b1a5ffb79afca5ee8999b8ea5b0aa07f
Tina <tinamahmoudi201@gmail.com>
2024-10-17 21:30:35 -0800

    -

--- src/Redux/rootStore.jsx
+++ src/Redux/rootStore.jsx
@@ -1,13 +1,12 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import CourseSlice from "./CourseSlice";
 import NewsSlice from "./NewsSlice";
-import { auth } from "../configs/router/auth.router";
-
+import auth from "./auth";
 const store = configureStore({
     reducer:{
         Courses:CourseSlice,
         news: NewsSlice,
-        auth : auth,
+        auth 
     },
 })
 
